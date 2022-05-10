fcc8575d1ab5d87110476e5a9c47daf45aa29257
taicout6 <taicout6@gmail.com>
2022-05-10 12:55:10 -0300

    Add Mirror of Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Counter from './components/counter'
+import Mirror from './components/mirror'
 
 export default function App() {
   return (
     <div className="App">
       <h1>Iniciando!</h1>
       <Counter />
+      <hr />
+      <Mirror />
     </div>
   )
 }
---
+++ src/components/mirror/index.jsx
@@ -0,0 +1,9 @@
+export default function Mirror() {
+  return (
+    <div>
+      <span>
+        <b>Mirror: 0</b>
+      </span>
+    </div>
+  )
+}
