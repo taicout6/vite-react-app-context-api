e6a2a4019ae542c8cec45f5ca3a8aaa641680d16
taicout6 <taicout6@gmail.com>
2022-05-10 13:40:26 -0300

    Add Themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import ThemeProvider from './context/Theme';
+import CountProvider from './context/count';
+import Container from './components/container';
 import Counter from './components/counter';
 import Mirror from './components/mirror';
-import CountProvider from './context/count';
 
 export default function App() {
   return (
-    <CountProvider>
-      <div className="App">
-        <h1>Context API xD</h1>
-        <Counter />
-        <hr />
-        <Mirror />
-      </div>
-    </CountProvider>
+    <ThemeProvider>
+      <CountProvider>
+        <Container>
+          <Counter />
+          <hr />
+          <Mirror />
+        </Container>
+      </CountProvider>
+    </ThemeProvider>
   );
 }
